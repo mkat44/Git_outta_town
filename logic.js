c9cda413595b47992a3dc8222ff2340df94aa1b9
Chase Holladay <chaseholladay@gmail.com>
2018-10-08 13:32:01 -0500

    working on adding parameters

--- logic.js
+++ logic.js
@@ -5,36 +5,6 @@
 // // API key for open weather app
 // cd6ea6b940f096174cde65479a31e8b5
 
-// var key = "cd6ea6b940f096174cde65479a31e8b5";
-// var city = "YOUR CITY"; // My test case was "London"
-// var url = "https://api.openweathermap.org/data/2.5/forecast?";
-
-// $.ajax({
-//   url: url, //API Call
-//   dataType: "json",
-//   type: "GET",
-//   data: {
-//     q: city,
-//     appid: key,
-//     units: "metric",
-//     cnt: "10"
-//   },
-//   success: function(data) {
-//     console.log('Received data:', data) // For testing
-//     var wf = "";
-//     wf += "<h2>" + data.city.name + "</h2>"; // City (displays once)
-//     $.each(data.list, function(index, val) {
-//       wf += "<p>" // Opening paragraph tag
-//       wf += "<b>Day " + index + "</b>: " // Day
-//       wf += val.main.temp + "&degC" // Temperature
-//       wf += "<span> | " + val.weather[0].description + "</span>";
-      
-//     });
-//     $("#showWeatherForcast").html(wf);
-//   }
-// });
-
- 
     $(document).ready(function(){
 
         $('#submitWeather').click(function(){
@@ -44,18 +14,20 @@
             if (city != ''){
     
                 $.ajax({
-                    url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + "&precipitation" + "&appid=cd6ea6b940f096174cde65479a31e8b5",
+                    url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + "&appid=cd6ea6b940f096174cde65479a31e8b5",
                     type: "GET",
                     dataType: "jsonp",
                     success: function(data){
                         var widget = show(data);
-    
+                        $("#show").text(data.main.temp);
                         $("#show").html(widget);
     
                         $("#city").val('');
                     }
+
                 });
-    
+            
+                
             }else{
                 $("#error").html('Field cannot be empty');
             }
